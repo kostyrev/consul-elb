b614870e95066a014fde14e2aed089ee5d6226c7
Aleksandr Kostyrev <kostyrevaa@gmail.com>
2017-10-17 19:57:26 +0300

    Remove allow_inbound_api rule

--- main.tf
+++ main.tf
@@ -68,15 +68,6 @@ resource "aws_security_group" "consul" {
   vpc_id      = "${var.vpc_id}"
 }
 
-resource "aws_security_group_rule" "allow_inbound_api" {
-  type              = "ingress"
-  from_port         = "${var.lb_port}"
-  to_port           = "${var.lb_port}"
-  protocol          = "tcp"
-  cidr_blocks       = ["${var.allowed_inbound_cidr_blocks}"]
-  security_group_id = "${aws_security_group.consul.id}"
-}
-
 resource "aws_security_group_rule" "allow_all_outbound" {
   type        = "egress"
   from_port   = 0
